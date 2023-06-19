661a1acb2e3631c6bc58198b2f662f6a043572a6
Vincent Allen <vallen813@gmail.com>
2023-06-19 15:25:17 -0700

    PlayerStateMachineInit
    Initial files set up for the player state machine

---
+++ Assets/Scenes/Vincent/Scripts/Concrete States/PlayerIdleState.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerIdleState : PlayerBaseState
+{
+   public override void EnterState(PlayerStateManager player) {
+      throw new System.NotImplementedException();
+   }
+    
+   public override void UpdateState(PlayerStateManager player) {
+      throw new System.NotImplementedException();
+   }
+    
+   public override void OnCollisionEnter(PlayerStateManager player, Collision collision) {
+      throw new System.NotImplementedException();
+   }
+}
