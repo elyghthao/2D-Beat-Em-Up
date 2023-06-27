7dc21ca7d10b21fdf2449ae70c13900ef2d66082
Brandon Jackson <brajackson64@gmail.com>
2023-06-27 16:19:53 -0700

    Player can now break crate, added simple crate sprite

--- Assets/Scripts/Brandon/Crate_Destroy.cs
+++ Assets/Scripts/Brandon/Crate_Destroy.cs
@@ -4,28 +4,32 @@ using UnityEngine;
 
 public class Crate_Destroy : MonoBehaviour
 {
-    AudioSource crate_break_sound; // The sound clip for when the crate is destroyed.
-    MeshRenderer crate_mesh_rend; // The mesh rendering of the crate.
-
-    bool crate_destroyed; // Tracks whether the crate is destroyed or not.
+    private AudioSource crate_break_sound; // The sound clip for when the crate is destroyed.
+    private MeshRenderer crate_mesh_rend; // The mesh rendering of the crate.
 
     // Start is called before the first frame update
     void Start()
     {
+        // On start, the crate's audio source and mesh renderer components are
+        // given to the variable for access.
         crate_break_sound = GetComponent<AudioSource>();
         crate_mesh_rend = GetComponent<MeshRenderer>();
-        crate_destroyed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // If the crate is no longer being rendered and crate_destroyed is set to false...
-        if (crate_mesh_rend.enabled == false && crate_destroyed == false)
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // If the crate comes into contact with the player's attack hitboxes,
+        // signified by the tag "Attack Hitbox", the crate stops being rendered
+        // and the crate breaking sound effect plays.
+        if (other.gameObject.CompareTag("Attack Hitbox"))
         {
-            // ...then play the crate destroyed sound clip and set crate_destroyed to true.
+            crate_mesh_rend.enabled = false;
             crate_break_sound.Play();
-            crate_destroyed = true;
         }
     }
 }
