2d39453bc34ac56874933797e32f9efeacfd1c1e
Brandon Jackson <brajackson64@gmail.com>
2023-07-04 15:56:21 -0700

    Modified objects to respond to all the player's attack tags.
    The crate, steel crate, and button are now destroyed/activated when an attack with any of the recently added attack hitbox tags collide with them.

--- Assets/Scripts/Brandon/Crate_Destroy.cs
+++ Assets/Scripts/Brandon/Crate_Destroy.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Crate_Destroy : MonoBehaviour
 {
+    // The list of attack hitbox tags to check for collision with the crate.
+    private string[] tags_to_check = {"FirstLightAttack", "SecondLightAttack", "ThirdLightAttack",
+                                    "FirstMediumAttack", "SecondMediumAttack", "SlamAttack", "Attack Hitbox"};
     private AudioSource crate_break_sound; // The sound clip for when the crate is destroyed.
     private BoxCollider crate_collider; // The crate's box collider.
     public MeshRenderer crate_mesh; // The crate's mesh renderer.
@@ -26,36 +29,41 @@ public class Crate_Destroy : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        // If the crate comes into contact with the player's attack hitboxes,
-        // signified by the tag "Attack Hitbox"...
-        if (other.gameObject.CompareTag("Attack Hitbox"))
+        // Every attack hitbox tag is used to check collision between the crate and
+        // the player's attack hitboxes.
+        foreach (string tag in tags_to_check)
         {
-            // ...then the crate breaking sound effect plays, and the crate's
-            // collider and mesh are both disabled.
-            crate_break_sound.Play();
-            crate_collider.enabled = false;
-            crate_mesh.enabled = false;
-
-            // If health_appear_guarantee is false...
-            if (health_appear_guarantee == false)
+            // If the crate comes into contact with the player's attack hitbox,
+            // signified by the currently compared tag...
+            if (other.gameObject.CompareTag(tag))
             {
-                // ...then a random number from 1 to 4 is chosen...
-                int health_appear_freq = Random.Range(1, 4);
-                
-                // ...and if the number is 1, then a health pack item is spawned.
-                if (health_appear_freq == 1)
+                // ...then the crate breaking sound effect plays, and the crate's
+                // collider and mesh are both disabled.
+                crate_break_sound.Play();
+                crate_collider.enabled = false;
+                crate_mesh.enabled = false;
+
+                // If health_appear_guarantee is false...
+                if (health_appear_guarantee == false)
                 {
+                    // ...then a random number from 1 to 4 is chosen...
+                    int health_appear_freq = Random.Range(1, 4);
+                    
+                    // ...and if the number is 1, then a health pack item is spawned.
+                    if (health_appear_freq == 1)
+                    {
+                        Instantiate(health_pack, transform.position, Quaternion.identity);
+                    }
+                }
+                else // if (health_appear_guarantee == true)
+                {
+                    // If health_appear_guarantee is true, then a health pack item is always spawned.
                     Instantiate(health_pack, transform.position, Quaternion.identity);
                 }
-            }
-            else // if (health_appear_guarantee == true)
-            {
-                // If health_appear_guarantee is true, then a health pack item is always spawned.
-                Instantiate(health_pack, transform.position, Quaternion.identity);
-            }
 
-            // Finally, the crate object is destroyed.
-            Destroy(gameObject, 1);
+                // Finally, the crate object is destroyed.
+                Destroy(gameObject, 1);
+            }
         }
     }
 }
--- Assets/Scripts/Brandon/Steel_Crate_Destroy.cs
+++ Assets/Scripts/Brandon/Steel_Crate_Destroy.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Steel_Crate_Destroy : MonoBehaviour
 {
+    // The list of attack hitbox tags to check for collision with the steel crate.
+    private string[] tags_to_check = {"FirstLightAttack", "SecondLightAttack", "ThirdLightAttack",
+                                    "FirstMediumAttack", "SecondMediumAttack", "SlamAttack", "Attack Hitbox"};
     private AudioSource crate_sfx; // The audio source used for the steel crate.
     private BoxCollider crate_collider; // The steel crate's box collider.
     public MeshRenderer crate_mesh; // The steel crate's mesh renderer.
@@ -35,49 +38,54 @@ public class Steel_Crate_Destroy : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        // If the steel crate comes into contact with the player's attack hitboxes,
-        // signified by the tag "Attack Hitbox"...
-        if (other.gameObject.CompareTag("Attack Hitbox"))
+        // Every attack hitbox tag is used to check collision between the steel crate
+        // and the player's attack hitboxes.
+        foreach (string tag in tags_to_check)
         {
-            // ...and the steel crate currently has only 1 health point remaining...
-            if (crate_health == 1)
+            // If the steel crate comes into contact with the player's attack hitbox,
+            // signified by the currently compared tag...
+            if (other.gameObject.CompareTag("Attack Hitbox"))
             {
-                // ...the the crate's audio clip changes to the crate breaking sound...
-                crate_sfx.clip = crate_break_sound;
-
-                // ...and then the crate breaking sound plays, and the steel crate's
-                // collider and mesh are both disabled.
-                crate_sfx.Play();
-                crate_collider.enabled = false;
-                crate_mesh.enabled = false;
-
-                // If health_appear_guarantee is false...
-                if (health_appear_guarantee == false)
+                // ...and the steel crate currently has only 1 health point remaining...
+                if (crate_health == 1)
                 {
-                    // ...then a random number from 1 to 4 is chosen...
-                    int health_appear_freq = Random.Range(1, 4);
-                
-                    // ...and if the number is 1, then a health pack item is spawned.
-                    if (health_appear_freq == 1)
+                    // ...then the crate's audio clip changes to the crate breaking sound...
+                    crate_sfx.clip = crate_break_sound;
+
+                    // ...and then the crate breaking sound plays, and the steel crate's
+                    // collider and mesh are both disabled.
+                    crate_sfx.Play();
+                    crate_collider.enabled = false;
+                    crate_mesh.enabled = false;
+
+                    // If health_appear_guarantee is false...
+                    if (health_appear_guarantee == false)
                     {
+                        // ...then a random number from 1 to 4 is chosen...
+                        int health_appear_freq = Random.Range(1, 4);
+                    
+                        // ...and if the number is 1, then a health pack item is spawned.
+                        if (health_appear_freq == 1)
+                        {
+                            Instantiate(health_pack, transform.position, Quaternion.identity);
+                        }
+                    }
+                    else // if (health_appear_guarantee == true)
+                    {
+                        // If health_appear_guarantee is true, then a health pack item is always spawned.
                         Instantiate(health_pack, transform.position, Quaternion.identity);
                     }
-                }
-                else // if (health_appear_guarantee == true)
-                {
-                    // If health_appear_guarantee is true, then a health pack item is always spawned.
-                    Instantiate(health_pack, transform.position, Quaternion.identity);
-                }
 
-                // Finally, the steel crate object is destroyed.
-                Destroy(gameObject, 1);
-            }
-            else // if (crate_health > 1)
-            {
-                // If the crate's health is still greater than 1, then its health
-                // decrements and the crate hurt sound plays.
-                crate_health--;
-                crate_sfx.Play();
+                    // Finally, the steel crate object is destroyed.
+                    Destroy(gameObject, 1);
+                }
+                else // if (crate_health > 1)
+                {
+                    // If the crate's health is still greater than 1, then its health
+                    // decrements and the crate hurt sound plays.
+                    crate_health--;
+                    crate_sfx.Play();
+                }
             }
         }
     }
