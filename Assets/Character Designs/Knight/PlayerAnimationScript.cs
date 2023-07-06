8c435290171e7cb4c1d12ae6594ffaed7597014f
elyghthao <elyghthao@gmail.com>
2023-07-05 19:54:19 -0700

    Knight prefab and girl prefab made

--- Assets/Character Designs/Knight/PlayerAnimationScript.cs
+++ Assets/Character Designs/Knight/PlayerAnimationScript.cs
@@ -5,15 +5,11 @@ using UnityEngine;
 public class PlayerAnimationScript : MonoBehaviour
 {
     private Animator anim;
-    public GameObject flipBone;
-    public bool isFacingRight = true;
     // Start is called before the first frame update
 
 
 
-    private float scaleX;
-    private float scaleY;
-    private float scaleZ;
+    
     void Start()
     {
         anim = this.GetComponent<Animator>();
@@ -37,31 +33,20 @@ public class PlayerAnimationScript : MonoBehaviour
             Input.GetKey(KeyCode.UpArrow)||
             Input.GetKey(KeyCode.LeftArrow)||
             Input.GetKey(KeyCode.RightArrow) ){
-                anim.Play("KnightWalking");
+                anim.Play("Walk");
             }else {
-                anim.Play("KnightIdle");
+                anim.Play("Idle");
             }
         }
 
 
         
-        if (Input.GetKey(KeyCode.LeftArrow)){//orientation
-            isFacingRight = false;
-        }else if (Input.GetKey(KeyCode.RightArrow)){
-            isFacingRight = true;
-        }
+        
 
 
 
 
 
-        scaleX = transform.localScale[0];
-        scaleY = transform.localScale[1];
-        scaleZ = transform.localScale[2];
-        if(!isFacingRight) {//is facing left
-            // flipBone.transform.localScale = new Vector3(1, -1, 1);
-        }else {
-            // flipBone.transform.localScale = new Vector3(1,1,1);
-        }
+        
     }
 }
