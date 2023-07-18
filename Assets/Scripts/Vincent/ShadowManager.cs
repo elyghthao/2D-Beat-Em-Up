19e9983f24abb5ae8202e9a4dc0a07ac6a770074
Vincent Allen <vallen813@gmail.com>
2023-07-18 16:35:59 -0700

    debug cleanup

--- Assets/Scripts/Vincent/ShadowManager.cs
+++ Assets/Scripts/Vincent/ShadowManager.cs
@@ -17,14 +17,12 @@ public class ShadowManager : MonoBehaviour {
     public Vector3 GetPointBelow() {
         RaycastHit hit;
         Vector3 curPos = host.transform.position;
-        Debug.DrawRay(new Vector3(curPos.x, curPos.y + host.transform.localScale.y * 0.75f, curPos.z), Vector3.down * 10f, Color.red);
+        //Debug.DrawRay(new Vector3(curPos.x, curPos.y + host.transform.localScale.y * 0.75f, curPos.z), Vector3.down * 10f, Color.red);
         if (Physics.Raycast(new Vector3(curPos.x, curPos.y + host.transform.localScale.y * 0.75f, curPos.z), Vector3.down, out hit, 10f)) {
-            Debug.Log("Hit");
             if (hit.collider.CompareTag("Ground")) {
                 return new Vector3(transform.position.x, hit.point.y + 0.01f, transform.position.z);
             }
         }
-        Debug.Log("Not hitting");
         return new Vector3(curPos.x, curPos.y + 0.1f, curPos.z);
     }
 }
