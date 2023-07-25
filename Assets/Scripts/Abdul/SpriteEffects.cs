bb2b0420aa6ddb889565d3293d3fe0c7b347523f
AAN-121 <abduln3@uw.edu>
2023-07-24 22:09:13 -0700

    Revert "Merge branch 'Elygh-Branch' into CameraSystem"
    This reverts commit c10cd5d97e8cba70483e20aaeade8e8d2396349e, reversing
    changes made to 6ed33fbac064964b198edeac85fab28f841bf9bc.

---
+++ Assets/Scripts/Abdul/SpriteEffects.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Used for the VFX such as the slam smoke effect
+/*
+HOW TO USE:
+Setup the effects in the actual character, then disable it.
+After that, add an EffectObject using the inspector and fill out the information.
+Lastly, just call the 'doEffect' method with the name of the effect and done!
+*/
+public class SpriteEffects : MonoBehaviour {
+    // ============================================ PUBLIC VARIABLES ============================================
+    [System.Serializable]
+    public class EffectObject {
+        public string name; // Effect name
+        public GameObject effect; // Actual effect on the player
+        public float length; // The length the effect lasts for
+        public bool attach; // If true, attaches the effect to the player
+    }
+
+    public EffectObject[] objects; // Objects
+
+    // ============================================ PRIVATE VARIABLES ============================================
+    private Dictionary<string, EffectObject> _effects = new Dictionary<string, EffectObject>(); // Stores information about each effect
+    private static Transform _effectsParent; // Used for effects that are not attached
+
+
+    // ============================================ PRIVATE METHODS ============================================
+    // Used to loop through the array and put each element in the dictionary
+    void Awake() {
+        for (int i = 0; i < objects.Length; i++) {
+            EffectObject obj = objects[i];
+            _effects.Add(obj.name, obj);
+        }
+
+        if (_effectsParent == null) {
+            GameObject newObj = new GameObject("_EFFECT");
+            _effectsParent = newObj.transform;
+        }
+    }
+
+    // ============================================ PUBLIC METHODS ============================================
+    public void doEffect(string name, bool flip) {
+        // Finding the effect
+        EffectObject obj = _effects[name];
+        if (obj == null) {
+            Debug.LogWarning("SpriteEffects: Given name '" + name + "' does not match any effect.");
+            return;
+        }  
+
+        // Getting correct parent
+        Transform objParent = _effectsParent;
+        Vector3 pos = obj.effect.transform.position;
+        if (obj.attach) {
+            objParent = transform;
+        }
+
+        // Creating effect
+        GameObject newEffect = Instantiate(obj.effect, objParent);
+        newEffect.transform.position = pos;
+        newEffect.SetActive(true);
+        Destroy(newEffect, obj.length);
+
+        // Checking flip
+        if (flip) {
+            newEffect.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+
+    public void doEffect(string name) { doEffect(name, false); }
+}
--- Assets/Scripts/Vincent/PlayerStateMachines/PlayerStateMachine.cs
+++ Assets/Scripts/Vincent/PlayerStateMachines/PlayerStateMachine.cs
@@ -19,7 +19,6 @@ public class PlayerStateMachine : MonoBehaviour {
    [Header("Body Elements")] public GameObject body;
 
    public int maxHealth = 100;
-   public bool gotHealed = false;
 
    [Header("Attack Boundaries")] public GameObject heavyAttackBounds;
 
@@ -107,57 +106,57 @@ public class PlayerStateMachine : MonoBehaviour {
    // Getters and Setters
    public Vector2 CurrentMovementInput {
       get {
-         if (InputSystem == null) return new Vector2(0, 0);
-         return InputSystem.CurrentMovementInput;
+         if (InputSys == null) return new Vector2(0, 0);
+         return InputSys.CurrentMovementInput;
       }
    }
 
    public bool IsMovementPressed {
       get {
-         if (InputSystem == null) return false;
-         return InputSystem.IsMovementPressed;
+         if (InputSys == null) return false;
+         return InputSys.IsMovementPressed;
       }
    }
    public bool IsActionPressed {
       get {
-         if (InputSystem == null) return false;
-         return InputSystem.IsActionPressed;
+         if (InputSys == null) return false;
+         return InputSys.IsActionPressed;
       }
    }
    public bool IsActionHeld {
       get {
-         if (InputSystem == null) return false;
-         return InputSystem.IsActionHeld;
+         if (InputSys == null) return false;
+         return InputSys.IsActionHeld;
       }
    }
    public bool IsLightAttackPressed {
       get {
-         if (InputSystem == null) return false;
-         return InputSystem.IsLightAttackPressed;
+         if (InputSys == null) return false;
+         return InputSys.IsLightAttackPressed;
       }
    }
    public bool IsMediumAttackPressed {
       get {
-         if (InputSystem == null) return false;
-         return InputSystem.IsMediumAttackPressed;
+         if (InputSys == null) return false;
+         return InputSys.IsMediumAttackPressed;
       }
    }
    public bool IsPowerupPressed {
       get {
-         if (InputSystem == null) return false;
-         return InputSystem.IsHeavyAttackPressed;
+         if (InputSys == null) return false;
+         return InputSys.IsHeavyAttackPressed;
       }
    }
    public bool IsBlockPressed {
       get {
-         if (InputSystem == null) return false;
-         return InputSystem.IsBlockPressed;
+         if (InputSys == null) return false;
+         return InputSys.IsBlockPressed;
       }
    }
    public bool IsBlockHeld {
       get {
-         if (InputSystem == null) return false;
-         return InputSystem.IsBlockHeld;
+         if (InputSys == null) return false;
+         return InputSys.IsBlockHeld;
       }
    }
    public PlayerBaseState CurrentState { get; set; }
@@ -169,7 +168,7 @@ public class PlayerStateMachine : MonoBehaviour {
    public AttackBoundsManager LightFirstFollowupBounds { get; private set; }
    public AttackBoundsManager LightSecondFollowupBounds { get; private set; }
    public Rigidbody Rigidbody { get; set; }
-   public InputSystem InputSystem { get; set; }
+   public InputSystem InputSys { get; set; }
    public bool CharacterFlipped { get; set; }
    /// Attacked Indicators
    public bool IsAttacked { get; private set; }
@@ -195,10 +194,12 @@ public class PlayerStateMachine : MonoBehaviour {
 
    public bool FinishedInitialization { get; private set; }
 
+   public SpriteEffects SpriteEffects { get; private set; }
+
    // Functions
 
    private void Awake() {
-      InputSystem = GameManager.Instance.gameObject.GetComponent<InputSystem>();
+      InputSys = GameManager.Instance.gameObject.GetComponent<InputSystem>();
       RecievedAttack[(int) Attacks.LightAttack1] = new AttackType("FirstLightAttack", new Vector2(1, 10), 40, 5);
       RecievedAttack[(int) Attacks.LightAttack2] = new AttackType("SecondLightAttack", new Vector2(1, 5), 60, 15);
       RecievedAttack[(int) Attacks.LightAttack3] = new AttackType("ThirdLightAttack", new Vector2(5, 10), 100, 30);
@@ -214,6 +215,7 @@ public class PlayerStateMachine : MonoBehaviour {
       LightFirstFollowupBounds = lightFirstFollowupAttackBounds.GetComponent<AttackBoundsManager>();
       LightSecondFollowupBounds = lightSecondFollowupAttackBounds.GetComponent<AttackBoundsManager>();
 
+      SpriteEffects = GetComponent<SpriteEffects>();
       Rigidbody = GetComponent<Rigidbody>();
       Rigidbody.freezeRotation = true;
 
@@ -233,7 +235,7 @@ public class PlayerStateMachine : MonoBehaviour {
       IsGrounded = CheckIfGrounded();
       if (FollowupTimer > 0) {
          FollowupTimer -= Time.deltaTime;
-         // Debug.Log("Followup Timer: " + FollowupTimer);
+         //Debug.Log("Followup Timer: " + FollowupTimer);
       }
    }
 
@@ -248,7 +250,7 @@ public class PlayerStateMachine : MonoBehaviour {
    ///    Disables all input for the character when the PlayerStateMachine script is disabled
    /// </summary>
    private void OnDisable() {
-      InputSystem.DisablePlayerInput();
+      InputSys.DisablePlayerInput();
    }
 
    private void OnDestroy() {
@@ -286,8 +288,8 @@ public class PlayerStateMachine : MonoBehaviour {
    }
 
    private IEnumerator SafeOnEnable() {
-      while (InputSystem == null || InputSystem.EmptyPlayerInput) yield return null;
-      InputSystem.EnablePlayerInput();
+      while (InputSys == null || InputSys.EmptyPlayerInput) yield return null;
+      InputSys.EnablePlayerInput();
    }
 
    public bool CheckIfGrounded() {
@@ -323,7 +325,7 @@ public class PlayerStateMachine : MonoBehaviour {
       // limit velocity if needed
       if (flatVelocity.magnitude > movementSpeed) {
          var limitedVelocity = flatVelocity.normalized * movementSpeed;
-         Rigidbody.velocity = new Vector3(limitedVelocity.x, 0f, limitedVelocity.z);
+         GetComponent<Rigidbody>().velocity = new Vector3(limitedVelocity.x, 0f, limitedVelocity.z);
       }
    }
 
@@ -334,13 +336,27 @@ public class PlayerStateMachine : MonoBehaviour {
       CharacterFlipped = !CharacterFlipped;
       // Debug.Log("Character flipped: " + _characterFlipped);
       transform.localScale = Vector3.Scale(transform.localScale, new Vector3(-1, 1, 1));
+
+      // Effect
+      int effectNumber = Random.Range(1, 3);
+      if (effectNumber == 1) {
+         SpriteEffects.doEffect("Direction", CharacterFlipped); 
+      } else {
+         SpriteEffects.doEffect("Direction2", CharacterFlipped); 
+      }
+
+      // NEW FLIP SYSTEM BELOW
+      // if (!CharacterFlipped) {
+      //     transform.localEulerAngles = new Vector3(0, 0, 0);
+      // } else {
+      //     transform.localEulerAngles = new Vector3(0, 180, 0);
+      // }
    }
 
    /// <summary>
    ///    Adds health to the player
    /// </summary>
    public void HealCharacter(int addedHealth) {
-      gotHealed = true;
       if (addedHealth <= 0) return;
       currentHealth += addedHealth;
       if (currentHealth > maxHealth) currentHealth = maxHealth;
