70bf6ab5684519b6cd211d08b10b1260087fd8d2
Brandon Jackson <brajackson64@gmail.com>
2023-07-26 18:30:30 -0700

    Placed enemies in their proper spots in each level,
    Also commentated the pause screen and background music scripts (again).

--- Assets/Scripts/Brandon/Background_Music.cs
+++ Assets/Scripts/Brandon/Background_Music.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 
 public class Background_Music : MonoBehaviour
 {
-    public AudioClip bgm_sound;
-    public AudioClip pause_sound;
-    public AudioClip game_over_sound;
+    public AudioClip bgm_sound; // The audio clip for the current level's background music.
+    public AudioClip pause_sound; // The audio clip for the pause screen music.
+    public AudioClip game_over_sound; // The audio clip for the game over sound effect.
 
     // Start is called before the first frame update
     void Start()
     {
+        // The scene starts by playing its associated background music.
         PlayBackgroundMusic();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void PlayBackgroundMusic()
     {
+        // The audio clip is changed to the level background music, which
+        // starts playing looped.
         GetComponent<AudioSource>().clip = bgm_sound;
         GetComponent<AudioSource>().loop = true;
         GetComponent<AudioSource>().Play();
@@ -29,6 +31,8 @@ public class Background_Music : MonoBehaviour
 
     public void PlayPauseMusic()
     {
+        // The audio clip is changed to the pause screen music, which
+        // starts playing looped.
         GetComponent<AudioSource>().clip = pause_sound;
         GetComponent<AudioSource>().loop = true;
         GetComponent<AudioSource>().Play();
@@ -36,6 +40,8 @@ public class Background_Music : MonoBehaviour
 
     public void PlayGameOverSound()
     {
+        // The audio clip is changed to the game over sound effect,
+        // which plays once.
         GetComponent<AudioSource>().clip = game_over_sound;
         GetComponent<AudioSource>().loop = false;
         GetComponent<AudioSource>().Play();
